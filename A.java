c560f696ced03a0ea02234d308154b9427eca128
Abimanyu190509 <gopinatharumugam19@gmail.com>
2020-03-06 11:58:48 +0530

    Project Design

---
+++ A.java
@@ -0,0 +1,19 @@
+package org.cts;
+
+public class A {
+	
+public static void main(String[] args) {
+	System.out.println("Hi");
+	System.out.println("Hello");
+	System.out.println("A");
+	System.out.println("B");
+	System.out.println("D");
+	System.out.println("E");
+	System.out.println("G");
+	System.out.println("F");
+	System.out.println("H");
+	System.out.println("C");
+	System.out.println("I");
+}
+
+}
